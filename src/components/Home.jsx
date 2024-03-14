21537db66b7136becfa85b94031643c549c693a4
VEINAL SEQUEIRA <veinalsequeira5229@gmail.com>
2024-03-14 23:40:17 +0530

    third commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,10 +13,11 @@ export default function Home() {
 
       {/* landing page */}
       <div className="flex w-full h-full mt-28">
-        <div className=" flex justify-center w-1/2 h-80 ml-10 mr-5 border-2 border-black" id="name">
+        <div className=" flex  w-4/6 h-80 ml-20 mr-5 border-2 border-black" id="name">
           <div>
-            <p className=" text-5xl font-bold">This is Veinal</p>
+            <p className=" text-6xl font-bold">Hi, I'm Veinal</p>
             <p className=" text-5xl font-bold">Full stack developer</p>
+            <p className=' text-lg mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quas velit accusamus adipisci odit hic esse quaerat quo sunt nostrum?</p>
           </div>
         </div>
         <div className=" flex justify-center w-1/2 h-80 mr-10 ml-5 border-2 border-black" id="photo-div">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,26 +4,33 @@ import { motion } from "framer-motion"
 export default function Projects() {
     return (
         <div>
-            <section className=" h-3/6 my-10 p-10 border-2 border-black bg-gray-950">
+            <section className=" h-3/6 my-10 py-10 border-2 border-black bg-gray-950">
                 <h1 className=" text-center text-4xl font-bold text-white mb-10">PROJECTS</h1>
-                <div className=" flex items-center justify-center gap-10">
-                    <div className=" w-60 h-60 bg-white hover:scale-105">
-
+                <div className=" flex-column items-center justify-center mx-20">
+                    <div className='flex gap-10 mb-10'>
+                        <div className=" w-2/6 h-60 bg-white hover:scale-105">
+    
+                        </div>
+                        <div className=" w-2/6 h-60 bg-white hover:scale-105">
+    
+                        </div>
+                        <div className=" w-2/6 h-60 bg-white hover:scale-105">
+    
+                        </div>
                     </div>
-                    <div className=" w-60 h-60 bg-white hover:scale-105">
-
-                    </div>
-                    <div className=" w-60 h-60 bg-white hover:scale-105">
-
-                    </div>
-                    <div className=" w-60 h-60 bg-white hover:scale-105">
-
-                    </div>
-                    <div className=" w-60 h-60 bg-white hover:scale-105">
-
+                    <div className='flex gap-10'>
+                        <div className=" w-2/6 h-60 bg-white hover:scale-105">
+    
+                        </div>
+                        <div className=" w-2/6 h-60 bg-white hover:scale-105">
+    
+                        </div>
+                        <div className=" w-2/6 h-60 bg-white hover:scale-105">
+    
+                        </div>
                     </div>
                 </div>
-                <span className=' flex justify-end '><button className=" bg-white border border-black py-2 px-4 mt-6 rounded-md text-lg">More..</button></span>
+                <span className=' flex justify-end mb-10 mr-10'><button className=" bg-white border border-black py-2 px-4 mt-6 rounded-md text-lg">More..</button></span>
             </section>
         </div>
     )
